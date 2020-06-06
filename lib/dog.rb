adf861ce16231e21106574a2cc56907f24c5a4c7
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-06 22:08:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,3 +6,25 @@ class Dog
     @this_dogs_name
   end
 end
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+
+
+
+
+7:08
+barking dog
+7:08
+class Dog
+def name=(name)
+  @name = name
+end
+def name
+  @name
+end
+def bark
+  puts "woof!"
+end
+end
